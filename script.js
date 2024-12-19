0afd67afeac1c417443395b0c47a77837d3a7f27
Brandon <93629403+hairforce1x@users.noreply.github.com>
2024-12-19 15:34:33 -0700

    Added styling to cards. Generated cards with for loop.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+// const cards = document.createElement("div")
+
+// document.body.appendChild(cards)
 // Step 1 generate cards using createElement
-
-
-
+for (i = 0; i<4; i++){
+    const cards = document.createElement("div") // create at least one elemnt using createElement
+    cards.classList.add("cards")
+    document.body.appendChild(cards) // use appendChild to add new elements to DOM
+}
 // Step 2 create function to handle click event of cards
 
 
