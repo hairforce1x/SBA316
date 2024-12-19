4961ac5290d782fe4c44bcb980cb30e8c6d7f553
Brandon <93629403+hairforce1x@users.noreply.github.com>
2024-12-19 15:16:51 -0700

    Added html boilerplate and basic outline of steps for functionality

---
+++ script.js
@@ -0,0 +1,17 @@
+// Step 1 generate cards using createElement
+
+
+
+// Step 2 create function to handle click event of cards
+
+
+
+// Step 3 compare cards
+
+
+
+// Step 4 check for condition (all cards flipped) that game is over
+
+
+
+// Step 5 Add simple form with validation
