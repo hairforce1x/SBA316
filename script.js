547cc360c57e02c7aac6a958d4cbf36bbf0d5633
Brandon <93629403+hairforce1x@users.noreply.github.com>
2024-12-20 17:28:42 -0700

    No idea.

--- script.js
+++ script.js
@@ -3,12 +3,13 @@
 // document.body.appendChild(cards)
 // Step 1 generate cards using createElement
 for (i = 0; i<4; i++){
-    const cards = document.createElement("div") // create at least one elemnt using createElement
-    cards.classList.add("cards")
+    const cards = document.createElement("div") // create at least one elemnt using createElementCache at least one element using querySelector
+    cards.classList.add("cards") 
     document.body.appendChild(cards) // use appendChild to add new elements to DOM
 }
+const allCards = document.querySelectorAll('.cards') // Cache at least one element using querySelector
+console.log(allCards)
 // Step 2 create function to handle click event of cards
-
 
 
 // Step 3 compare cards
