522e8c8de47bd855ffc67519c07f17cfd6d8ecdc
Brandon <93629403+hairforce1x@users.noreply.github.com>
2024-12-20 18:08:01 -0700

    Added eventListener and textContent criteria

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-// const cards = document.createElement("div")
-
-// document.body.appendChild(cards)
 // Step 1 generate cards using createElement
-for (i = 0; i<4; i++){
-    const cards = document.createElement("div") // create at least one elemnt using createElementCache at least one element using querySelector
-    cards.classList.add("cards") 
+for (i = 0; i<2; i++){
+    const cards = document.createElement("div") // create at least one elemnt using createElement
+    cards.classList.add('cards')
     document.body.appendChild(cards) // use appendChild to add new elements to DOM
 }
 const allCards = document.querySelectorAll('.cards') // Cache at least one element using querySelector
 console.log(allCards)
+
 // Step 2 create function to handle click event of cards
+allCards.forEach(card => {
+    card.addEventListener('click', cardClick); // 
+})
 
+function cardClick(e) {
+    const clickedCard = e.target;
+    clickedCard.classList.add('clicked');
+    clickedCard.textContent = 'clicked'; // Modify the HTML or text content of at least one element in response to user interaction using innerHTML, innerText, or textContent.
+    console.log('clicked:', clickedCard);
+}
 
-// Step 3 compare cards
+// Step 3 compare two cards
 
 
 
